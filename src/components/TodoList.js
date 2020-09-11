376a6395a0ac244f9936882329887e23514539c8
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-09-11 14:57:59 +0900

    react-virtualized 사용 눈에 보이는 부분만 렌더링하기

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
+import {List} from 'react-virtualized';
 
 //todos라는 props을 받아옴
 const TodoList = ({todos, onRemove, onToggle}) => {
-    return (
-        <div className="TodoList">
-            {todos.map(todo => ( //map함수로 반복문, todo라는 매개변수를 받아서 TodoListItem을 todos 배열 크기만큼 map함수는 key props가 필요함
+    const rowRenderer = useCallback(
+        ({index, key, style})=> {
+            const todo = todos[index];
+            return (
                 <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-            ))}
-        </div>
+            );
+        },[onRemove, ontoggle, todos],
+    )
+    return (
+        <List 
+            className="TodoList"
+            width={512}
+            height={513}
+            rowCount={todos.length}
+            rowHeight={57}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{outline:'none'}}
+        />
     );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
